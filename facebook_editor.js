b2c2f262c8140f7c2109ea800033384dd1d9a9b5
Hoàng Anh <75006921+maky999@users.noreply.github.com>
2022-01-20 14:05:22 +0700

    Add files via upload

---
+++ facebook_editor.js
@@ -0,0 +1,14 @@
+clear();
+
+function doScaledTimeout(i) {
+    var t = Math.floor((Math.random() * 4) + 1);
+    var tok = t * 1000;
+    var ie = Math.floor(Math.random() * 1);
+    var aeq = [".j23y7wrc>div", ".j23y7wrc.scb9dxdr.dflh9lhu>div"];
+    setTimeout(function () {
+        document.querySelector(aeq[ie]).click();
+    }, i + tok);
+}
+for (let i = 1; i <= 5000; i++) {
+    doScaledTimeout(i)
+}
