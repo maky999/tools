950f0e2a6faca7e9780b23599a4faf5e7bca5542
Hoàng Anh <75006921+maky999@users.noreply.github.com>
2022-01-20 15:01:25 +0700

    Create email_canva.js

---
+++ email_canva.js
@@ -0,0 +1,4 @@
+var items = document.getElementsByClassName('_1QoxDw Qkd66A fFOiLQ NT2yCg Qkd66A fFOiLQ lsXp-w GnpDIA zQlusQ uRvRjQ');
+for (var i = 0; i < items.length; i++) {      
+  items[i].click();
+}
