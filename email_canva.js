7325a7ce4a0d0d878f1c15e977f34c4089fcb0e0
Hoàng Anh <75006921+maky999@users.noreply.github.com>
2022-01-20 15:32:34 +0700

    Update email_canva.js

--- email_canva.js
+++ email_canva.js
@@ -2,3 +2,5 @@ var items = document.getElementsByClassName('NT2yCg');
 for (var i = 0; i < items.length; i++) {      
   items[i].click();
 }
+
+//hoac don gian sd cái này-  document.querySelector(".NT2yCg").click();
