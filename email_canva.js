00704dc9d9c17cabcaf2bf002f93997c0186eebe
Hoàng Anh <75006921+maky999@users.noreply.github.com>
2022-09-07 09:36:20 +0700

    Update email_canva.js

--- email_canva.js
+++ email_canva.js
@@ -1,7 +1,7 @@
 //auto click gioi han 1000 milliseconds, 0 là không giới hạn
 setInterval(() => {
- document.querySelector(".NT2yCg").click();
+ document.querySelector("._1QoxDw.Qkd66A.tYI0Vw.o4TrkA.NT2yCg.Qkd66A.tYI0Vw.lsXp-w.cwOZMg.zQlusQ.uRvRjQ._3r-82w").click();
     },1000);
 
 // click 1 lan
-document.querySelector(".NT2yCg").click();
+document.querySelector("._1QoxDw.Qkd66A.tYI0Vw.o4TrkA.NT2yCg.Qkd66A.tYI0Vw.lsXp-w.cwOZMg.zQlusQ.uRvRjQ._3r-82w").click();
